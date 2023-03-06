9beecb501cfd98235dcdd70a2c1c55be0b174cc0
dustinyangfan <dustin.yang@nagarro.com>
2023-03-06 16:31:45 +0800

    add: add order list page

---
+++ mockData/orderList/orderList.js
@@ -0,0 +1,67 @@
+
+export const orderListData = [
+    {
+        "id": "0260855311",
+        "POId": "Q-00949113",
+        "orderData": "2023-02-21",
+        "deliveryParty": "成都市 610000",
+        "orderStatus": "已预订",
+        "orderAmount": "￥ 494,756.88"
+    },
+    {
+        "id": "0260855312",
+        "POId": "Q-00949114",
+        "orderData": "2023-02-20",
+        "deliveryParty": "Chengdu 610000",
+        "orderStatus": "准备装运",
+        "orderAmount": "￥ 561,211.22"
+    },
+    {
+        "id": "0260855422",
+        "POId": "Q-00949515",
+        "orderData": "2023-02-19",
+        "deliveryParty": "成都市 610000",
+        "orderStatus": "准备装运",
+        "orderAmount": "￥ 34,756.34"
+    },
+    {
+        "id": "0260855300",
+        "POId": "Q-00922113",
+        "orderData": "2023-02-11",
+        "deliveryParty": "成都市 610000",
+        "orderStatus": "发货完成",
+        "orderAmount": "￥ 887,444.12"
+    },
+    {
+        "id": "0260835313",
+        "POId": "Q-00949001",
+        "orderData": "2023-02-01",
+        "deliveryParty": "成都 610000",
+        "orderStatus": "部分发货",
+        "orderAmount": "￥ 112,223.34"
+    },
+    {
+      "id": "0260811223",
+      "POId": "Q-00912123",
+      "orderData": "2023-01-01",
+      "deliveryParty": "成都 610000",
+      "orderStatus": "部分发货",
+      "orderAmount": "￥ 332,111.22"
+    },
+    {
+      "id": "02608112231",
+      "POId": "Q-00912123",
+      "orderData": "2023-01-01",
+      "deliveryParty": "成都 610000",
+      "orderStatus": "部分发货",
+      "orderAmount": "￥ 332,111.22"
+    },
+    {
+      "id": "02608112232",
+      "POId": "Q-00912123",
+      "orderData": "2023-01-01",
+      "deliveryParty": "成都 610000",
+      "orderStatus": "部分发货",
+      "orderAmount": "￥ 332,111.22"
+    },
+]
